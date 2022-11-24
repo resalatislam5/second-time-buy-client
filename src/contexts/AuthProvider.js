f5f489b1069e1adbf6490f52dda9b5968fd6f8a3
Resalat <resalatislam5@gmail.com>
2022-11-25 00:54:38 +0600

    authProvider

---
+++ src/contexts/AuthProvider.js
@@ -0,0 +1,60 @@
+import React, { createContext, useEffect, useState } from 'react';
+import app from '../firebase/firebse.init';
+import { getAuth, createUserWithEmailAndPassword, updateProfile, onAuthStateChanged, signOut, signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
+
+const auth = getAuth(app)
+export const AuthContext = createContext()
+const AuthProvider = ({children}) => {
+    const [user,SetUser] = useState(null)
+    const [loading,setLoading] = useState(true);
+    //signUp
+    const SignUpEmail = (email,password) =>{
+        setLoading(true)
+        return createUserWithEmailAndPassword(auth,email,password)
+    }
+    //login
+    const LoginEmail = (email,password) =>{
+        setLoading(true)
+        return signInWithEmailAndPassword(auth,email,password)
+    }
+    //updateName
+    const updateName = (name) =>{
+        setLoading(true)
+        return updateProfile(auth.currentUser, {
+            displayName: name,
+          })
+    }
+    //SignOut
+    const logOut = () =>{
+        return signOut(auth)
+    }
+    //google login
+    const handleGoogleLogin = (GoogleProvider) =>{
+        setLoading(true)
+        return signInWithPopup(auth, GoogleProvider)
+    }
+    //OnAuthStateChange
+    useEffect(()=>{
+        const unSubscribe = onAuthStateChanged(auth, currentUser =>{
+            SetUser(currentUser)
+            setLoading(false)
+        })
+        return () => unSubscribe()
+    },[])
+    const authInfo = {
+        user,
+        loading,
+        SignUpEmail,
+        LoginEmail,
+        logOut,
+        handleGoogleLogin,
+        updateName,
+    }
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {QueryClient,QueryClientProvider} from '@tanstack/react-query'
+import AuthProvider from './contexts/AuthProvider';
 const queryClient = new QueryClient()
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-          <App />
-    </QueryClientProvider>
+    <AuthProvider>
+      <QueryClientProvider client={queryClient}>
+            <App />
+      </QueryClientProvider>
+    </AuthProvider>
   </React.StrictMode>
 );
 
