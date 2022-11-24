133cca0256d4050209c7e73d25ed972e80cabea8
Resalat <resalatislam5@gmail.com>
2022-11-24 14:09:22 +0600

    footer

--- src/Layout/Main.js
+++ src/Layout/Main.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Footer from '../pages/Sheard/Footer/Footer';
 import Header from '../pages/Sheard/Header/Header';
 
 
@@ -8,6 +9,7 @@ const Main = () => {
         <div>
             <Header />
             <Outlet />
+            <Footer />
         </div>
     );
 };
