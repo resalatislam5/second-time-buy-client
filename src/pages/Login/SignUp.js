45b641856a264dd9ebfa38e031f04568371853d7
Resalat <resalatislam5@gmail.com>
2022-11-25 03:12:35 +0600

    signup image

--- src/pages/Login/SignUp.js
+++ src/pages/Login/SignUp.js
@@ -3,6 +3,7 @@ import { FaFacebook,FaGoogle } from "react-icons/fa";
 
 const SignUp = () => {
     const [seller, setSeller] = useState('user')
+    const [images,setImages] = useState(null)
     const handleSignUp = e =>{
         e.preventDefault()
         const form = e.target;
@@ -10,15 +11,25 @@ const SignUp = () => {
         const email = form.email.value;
         const password = form.password.value;
         const location = form.location.value;
+        const image = form.image.files[0];
         const user = {
             name: name,
             email,
-            password: password,
             location: location,
             role: seller,
+            image:images
         }
-
-       
+        const data = new FormData()
+        data.append('image', image)
+        fetch(`https://api.imgbb.com/1/upload?key=${process.env.REACT_APP_imgbd_key}`,{
+            method:'POST',
+            body:data
+        })
+        .then(res=> res.json())
+        .then(data =>{
+            setImages(data.data.url)
+            
+        }).catch(err => console.log(err))
     }
     return (
         <div className='min-h-[50vh] mb-[1%]'>
@@ -33,7 +44,7 @@ const SignUp = () => {
                     </div>
                     <input className='border p-3 rounded-lg my-5' type="password" name="location" placeholder='Enter Your location' id="" required/>
                     <input className='border p-3 rounded-lg' type="password" name="password" placeholder='Enter Your Password' id="" required/>
-                    <input className='border p-3 rounded-lg my-5' type="password" name="confirm" placeholder='Enter Your confirm Password' id="" required/>
+                    <input type="file" id="image" className="file-input w-full max-w-xs my-5" />
                     <input className='border p-3 btn' type="submit" value="Register" />
                 </form>
                 <div className='flex justify-center mt-5 gap-5 text-3xl text-[#f75353]'>
