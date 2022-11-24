93a7460362d3b26eb888e604dd0a9d330d173f8f
Resalat <resalatislam5@gmail.com>
2022-11-24 13:48:54 +0600

    header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import { RouterProvider } from 'react-router-dom';
 import './App.css';
+import { routes } from './Routes/routers';
 
 function App() {
   return (
     <div className="App">
-    
+        <RouterProvider router={routes} />
     </div>
   );
 }
---
+++ src/Routes/routers.js
@@ -0,0 +1,17 @@
+import Main from "../Layout/Main";
+import Home from "../pages/Home/Home";
+
+const { createBrowserRouter } = require("react-router-dom");
+
+export const routes = createBrowserRouter([
+    {
+        path:'/',
+        element: <Main />,
+        children: [
+            {
+                path: '/',
+                element:<Home />
+            }
+        ]
+    }
+])
